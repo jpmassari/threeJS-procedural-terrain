ed7bbf86fc164fb0fcbca196e7cc95c33c8bea9b
João Pedro Massari <joao.massari1997@gmail.com>
2022-08-08 23:44:50 -0300

    (#feat/220115) prodecural-mesh
    Detalhes:
    Finalização da Mesh.

--- src/components/procedural-mesh.tsx
+++ src/components/procedural-mesh.tsx
@@ -1,32 +1,30 @@
 import { useEffect, useState, useRef, useCallback } from 'react';
-import styled from 'styled-components';
 import * as THREE from 'three';
-
-
 import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise';
-
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
 import { MeshContainer } from './procedural-mesh-loader';
 import { useEffectOnce } from './hooks/UseEffectOnce';
+import { DoubleSide, FlatShading, WireframeGeometry } from 'three';
+
 
 let cols = 0;
 let rows = 0;
-const scale = 20;
-const w = 120;
-const h = 80;
-let terrain:any[] = [];
-let flying = 0;
+const scale = 25;
+const w = 400;
+const h = 400;
 let count = 0;
+let flying = 0;
+
 const map_range = (value:number, low1:number, high1:number, low2:number, high2:number)  => {
   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
 const ProceduralMesh: React.FC = () => {
   const refContainer = useRef<HTMLCanvasElement>();
-  const [loading, setLoading ] = useState<boolean>(true);
+  const [ loading, setLoading ] = useState<boolean>(true);
   const [ renderer, setRenderer ] = useState<THREE.WebGLRenderer>();
-  const [ _camera, setCamera ] = useState<THREE.PerspectiveCamera>();
-  const [ target]  = useState<THREE.Vector3>(new THREE.Vector3(-0.5, 1.2, 0));
-
+  const [ camera, setCamera ] = useState<THREE.PerspectiveCamera>();
+  const [ target ]  = useState<THREE.Vector3>(new THREE.Vector3(-0.5, 1.2, 0));
   const [ scene ] = useState<THREE.Scene>(new THREE.Scene());
 
   const handleWindowResize = useCallback(() => {
@@ -43,7 +41,6 @@ const ProceduralMesh: React.FC = () => {
     geometry.geometry = new THREE.PlaneGeometry()
   }  
  */
-/* eslint-disable react-hooks/exhaustive-deps */
   useEffectOnce(() => {
     const { current: container } = refContainer;
     cols = w/scale;
@@ -53,107 +50,71 @@ const ProceduralMesh: React.FC = () => {
     if(container && !renderer) {
       const scW = container.clientWidth;
       const scH = container.clientHeight;
-
       const renderer = new THREE.WebGLRenderer({ antialias: true })
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(scW, scH);
       renderer.outputEncoding = THREE.sRGBEncoding;
+
+      const pmremGenerator = new THREE.PMREMGenerator( renderer );
+      scene.background = new THREE.Color( 0x000000 );
+			scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+
       //Updating the DOM with plain JavaScript
       container.appendChild(renderer.domElement);
       setRenderer(renderer);
 
       const camera = new THREE.PerspectiveCamera(75, scW / scH, 0.1, 1000);
-      camera.position.setZ(300);
-      /*      
-      const camera = new THREE.PerspectiveCamera( 60, scW / scH, 0.1, 100 );
-			camera.position.set( 10 * 0.9, 10 * 0.9, 10 * 0.9 );
-      camera.position.setZ(2);
-			camera.lookAt( 0, 0, 0 ); 
-      */
+      camera.position.setZ(800);
+      camera.position.setX(-300);
       setCamera(camera);
 
-      const disposeArray = () => null
+      const ambientLight = new THREE.AmbientLight( 0x000000 );
+			scene.add( ambientLight );
       
-      const material = new THREE.MeshBasicMaterial({
-        color: 0xFF6347, 
-        wireframe: true
-      });
+			const light1 = new THREE.PointLight( 0xffffff, 1, 0);
+			light1.position.set( -10, 0, 150 );
+			scene.add( light1 );
 
-      let v3 = new THREE.Vector3();
-      let v2 = new THREE.Vector2();
+			const light2 = new THREE.PointLight( 0xffffff, 1, 0 );
+			light2.position.set( 100, 200, 100 );
+			scene.add( light2 );
+
+			const light3 = new THREE.PointLight( 0xffffff, 1, 0 );
+			light3.position.set( - 100, - 200, - 100 );
+			scene.add( light3 );
+
+    /*   const material = new THREE.MeshBasicMaterial({
+        color: 0xFFFFFF, 
+        wireframe: true,
+      });
+       */
       
       const geometry = new THREE.PlaneGeometry( w, h, cols, rows );
-      /* geometry.rotateX(-Math.PI *0.5); */
-      geometry.rotateX(Math.PI/3)
+      geometry.rotateX(Math.PI/2);
+      geometry.rotateY(-Math.PI/4 + 20);
+      const material = new THREE.MeshPhysicalMaterial({ 
+        color: 0x049EF4,
+        reflectivity: 0,
+        transmission: 1,
+        roughness: 0,
+        metalness: 1,
+        clearcoat: 0.25,
+        clearcoatRoughness: 0,
+        ior: 0.5,
+        side: DoubleSide,
+        wireframe: false,
+        flatShading: true
+      })
       const mesh = new THREE.Mesh( geometry, material );
+      mesh.rotateZ(0.1);
+      mesh.rotateX(0.1);
       scene.add( mesh );
-      /* const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff }); */
-  /*     for(let y = 0; y < rows; y++) {
-        for (let x = 0; x < cols; x++) {
-          
-          geometry.rotateX(Math.PI/2);
-          const vertices = new Float32Array([
-            -x*scale, -y*scale, 1.0,
-            x*scale, -y*scale, 1.0,
-            x*scale, y*scale, 1.0,
 
-            x*scale, y*scale, 1.0,
-            -x*scale, y*scale, 1.0,
-            -x*scale, -y*scale, 1.0,
-          ])
-          geometry.setAttribute( 'position', new THREE.BufferAttribute(vertices, 3, false).onUpload(disposeArray));
-          const mesh = new THREE.Mesh( geometry, material );
-          scene.add( mesh );
-        }
-      }  */
-
-    /*   const geometry = new THREE.PlaneGeometry( w, h, cols, rows );
-      geometry.rotateX(Math.PI / 3);
-      const vertices = geometry.attributes.position?.array; */
-/*       for(let y = 0; y < rows; y++) {
-          terrain.push([]);
-          let xoff = 0;
-          for(let x = 0; x < cols; x++) {
-            terrain[y][x] = map_range(perlin.noise(xoff,yoff, z), 0, 5, -100, 100);
-            xoff += 0.2;
-          }
-          yoff += 0.2;
-      }
-      for(let y = 0; y < rows - 1; y++) {
-        for (let x = 0; x < cols; x++) {
-          geometry.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array( [x*scale, y*scale, terrain[y][x]] ), 3 ) );
-          const edges = new THREE.EdgesGeometry( geometry );
-          const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
-        }
-        p5.endShape();
-      }  */
       let req = null as unknown | number;
-      let frame = 0;
       const animate = () => {
         req = requestAnimationFrame(animate);
+        render(geometry);
         renderer.render(scene, camera);
-        const perlin = new ImprovedNoise(), z = Math.random() * 100;
-        frame += 0.1;
-        flying -= 0.1;
-        let yoff = flying;
-        for(let y = 0; y < rows; y++) {
-          terrain.push([]);
-          let xoff = 0;
-          for(let x = 0; x < cols; x++) {
-            terrain[y][x] = map_range(perlin.noise(xoff,yoff, z), 0, 5, -100, 100);
-            xoff += 0.2;
-          }
-          yoff += 0.2;
-        }
-        if(geometry.attributes.position) {
-          for(let y = 0; y < rows - 1; y++) {
-            for(let x = 0; x < cols; x++) {
-              geometry.attributes.position.setY(x, terrain[y][x]);
-            }
-          }
-          geometry.computeVertexNormals();
-          geometry.attributes.position.needsUpdate = true;
-        }
       }
       animate();
       return () => {
@@ -164,7 +125,25 @@ const ProceduralMesh: React.FC = () => {
       }
     }
   });
-
+  const render = (geometry:THREE.PlaneGeometry) => {
+		const time = Date.now() * 0.0001;
+    const perlin = new ImprovedNoise(), z = time;
+    const position = geometry.attributes.position;
+    flying += 0.001;
+    if(position) {
+      let xoff = Math.sin( time / 4 );
+      let yoff = Math.sin( time / 2) + flying;
+        for(let i = 0; i < position.count; i++) {
+          /* const y = 35 * Math.sin( i / 5 + ( time + i ) / 7 ); */
+          position.setY(i, map_range(perlin.noise(xoff, yoff, 30), 0, 10, -100, 100));
+          xoff += Math.sin(0.2);
+          yoff += 0.2;
+        }
+      geometry.computeVertexNormals();
+      position.needsUpdate = true;
+    }
+    renderer?.render( scene,  camera as THREE.PerspectiveCamera );
+  }
   useEffect(() => {
     window.addEventListener('resize', handleWindowResize, false)
     return () => {
